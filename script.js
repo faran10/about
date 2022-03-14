16ee3b19cb8daa8078ae59fa257545ae57a7b507
Faran Mujtaba <farans552@gmail.com>
2022-03-14 14:38:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function robincont() {
+    document.getElementById("contwithrobin").style.display = "block";
+}
+
+function katcont() {
+    document.getElementById("contwithkat").style.display = "block";
+}
+
+function richardcont() {
+    document.getElementById("contwithrichard").style.display = "block";
+}
+
+function monicont() {
+    document.getElementById("contwithmonica").style.display = "block";
+}
+
+// close
+
+function close1() {
+    document.getElementById("contwithrobin").style.display = "none";
+}
+
+function close2() {
+    document.getElementById("contwithkat").style.display = "none";
+}
+function close3() {
+    document.getElementById("contwithrichard").style.display = "none";
+}
+function close4() {
+    document.getElementById("contwithmonica").style.display = "none";
+}
